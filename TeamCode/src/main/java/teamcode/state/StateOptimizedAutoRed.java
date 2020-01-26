23e4bac1080568f69e91f36eaa7f910bba857ec1
MrJamesGaming <52509738+MrJamesGaming@users.noreply.github.com>
2020-01-25 17:48:10 -0800

    red auto?

--- TeamCode/src/main/java/teamcode/state/StateOptimizedAutoRed.java
+++ TeamCode/src/main/java/teamcode/state/StateOptimizedAutoRed.java
@@ -14,7 +14,7 @@ import teamcode.common.Vector2D;
 import teamcode.state.VisionOnInit.SkystonePos;
 
 @Autonomous(name="Blue auto")
-public class StateOptimizedAutoBlue extends AbstractOpMode {
+public class StateOptimizedAutoRed extends AbstractOpMode {
     DriveSystem drive;
     MoonshotArmSystem arm;
     VisionOnInit vision;
@@ -25,14 +25,14 @@ public class StateOptimizedAutoBlue extends AbstractOpMode {
     private final double SPEED = 1.0;
     @Override
     protected void onInitialize() {
-        gps = new GPS(hardwareMap, new Vector2D(9, 38.5), 0);
-        drive = new DriveSystem(hardwareMap, gps, new Vector2D(9, 38.5), 0);
+        gps = new GPS(hardwareMap, new Vector2D(-9, 38.5), Math.toRadians(180));
+        drive = new DriveSystem(hardwareMap, gps, new Vector2D(-9, 38.5), Math.toRadians(180));
         vision = new VisionOnInit(hardwareMap);
         arm = new MoonshotArmSystem(hardwareMap);
         timer1 = new Timer();
         timer2 = new Timer();
         while(!opModeIsActive()){
-            pos = vision.vuforiascan(false, false);
+            pos = vision.vuforiascan(false, true);
             Debug.log(pos);
         }
     }
@@ -64,27 +64,27 @@ public class StateOptimizedAutoBlue extends AbstractOpMode {
     private void scoreStone( boolean pullFoundation) {
         if(pullFoundation){
             //y value at 122
-            drive.goTo(new Vector2D(30, 113), SPEED);
-            drive.setRotation(Math.toRadians(180), SPEED);
+            drive.goTo(new Vector2D(-30, 113), SPEED);
+            drive.setRotation(Math.toRadians(0), SPEED);
 
-            drive.goTo(new Vector2D(41, 113), 0.3);
+            drive.goTo(new Vector2D(-41, 113), 0.3);
             arm.adjustFoundation();
             arm.scoreAUTO();
-            drive.goTo(new Vector2D(36, 113), SPEED);
+            drive.goTo(new Vector2D(-36, 113), SPEED);
             //drive.goTo(new Vector2D(18, 114), SPEED);
             //drive.goTo(new Vector2D(36,96), SPEED);
-            drive.setRotation(Math.toRadians(270), SPEED);
-            drive.goTo(new Vector2D(36,96), SPEED);
+            drive.setRotation(Math.toRadians(90), SPEED);
+            drive.goTo(new Vector2D(-36,96), SPEED);
             arm.adjustFoundation();
         }else{
-            drive.goTo(new Vector2D(24, 113), SPEED);
+            drive.goTo(new Vector2D(-24, 113), SPEED);
             arm.scoreAUTO();
         }
         //drive.goTo(new Vector2D(36, 96), SPEED);
         //drive.goTo(parkingSpot, SPEED);
     }
 
-    Vector2D parkingSpot = new Vector2D(36, 72);
+    Vector2D parkingSpot = new Vector2D(-36, 72);
 
     private void intakeStone(double stoneNum, boolean isFirst) {
 
@@ -92,7 +92,7 @@ public class StateOptimizedAutoBlue extends AbstractOpMode {
             intakeStone();
 //            drive.goTo(new Vector2D(24, 62), SPEED);
 //            drive.setRotation(Math.toRadians(-30), SPEED);
-            drive.goTo(new Vector2D(49, 52), 0.4);
+            drive.goTo(new Vector2D(-49, 52), 0.4);
         } else {
             //10, half the robot +1
             double stoneNumToInch = Constants.STONE_LENGTH_INCHES * (7 - stoneNum) + 10;
@@ -100,17 +100,17 @@ public class StateOptimizedAutoBlue extends AbstractOpMode {
 //            drive.goTo(new Vector2D(57, 37 - ((6 - stoneNum) - 2) * 8), 0.8);
 //            intakeStone();
 //            drive.goTo(new Vector2D(57, 37 - ((6 - stoneNum) - 1.5) * 8), SPEED);
-            drive.goTo(new Vector2D(36, stoneNumToInch), SPEED);
-            drive.goTo(new Vector2D(57, stoneNumToInch), 0.8);
+            drive.goTo(new Vector2D(-36, stoneNumToInch), SPEED);
+            drive.goTo(new Vector2D(-57, stoneNumToInch), 0.8);
             if(isFirst) {
                 drive.setRotation(Math.toRadians(90), SPEED);
             }
             intakeStone();
-            drive.goTo(new Vector2D(57, stoneNumToInch - 4), SPEED);
+            drive.goTo(new Vector2D(-57, stoneNumToInch - 4), SPEED);
         }
 
         //Utils.sleep(250);
-        drive.goTo(new Vector2D(36, 48), SPEED);
+        drive.goTo(new Vector2D(-36, 48), SPEED);
 
 
     }
